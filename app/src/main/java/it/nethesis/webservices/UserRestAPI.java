773f3944bc4ee80694adab179502a43c2c749e50
Simone Raffaelli <s.raffaelli@wedoit.io>
2021-02-01 12:26:53 +0100

    Add new API model

--- app/src/main/java/it/nethesis/webservices/UserRestAPI.java
+++ app/src/main/java/it/nethesis/webservices/UserRestAPI.java
@@ -1,9 +1,12 @@
 package it.nethesis.webservices;
 
 import it.nethesis.models.NethUser;
+import it.nethesis.models.contactlist.ContactList;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 /** Interface for Users Rest Api Calls. */
 public interface UserRestAPI {
@@ -15,4 +18,17 @@ public interface UserRestAPI {
      */
     @GET("user/me")
     Call<NethUser> getMe(@Header("Authorization") String authorizationHeader);
+
+    @GET("phonebook/search/{term}?view=name")
+    Call<ContactList> searchWithTerms(
+            @Header("Authorization") String authorizationHeader,
+            @Path("term") String term,
+            @Query("limit") int limit,
+            @Query("offset") int offset);
+
+    @GET("phonebook/search/{term}?view=name&limit=5")
+    Call<ContactList> searchWithTerms(
+            @Header("Authorization") String authorizationHeader,
+            @Path("term") String term,
+            @Query("offset") int offset);
 }
