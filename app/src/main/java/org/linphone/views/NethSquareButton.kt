b40e1b2923e04f0f78eef74150af60508706bac8
Simone Raffaelli <s.raffaelli@wedoit.io>
2021-06-17 18:12:07 +0200

    resize circular button based on screen size

---
+++ app/src/main/java/org/linphone/views/NethSquareButton.kt
@@ -0,0 +1,25 @@
+package org.linphone.views
+
+import android.content.Context
+import android.util.AttributeSet
+import com.google.android.material.button.MaterialButton
+import org.linphone.R
+import kotlin.math.min
+
+open class NethSquareButton @JvmOverloads constructor(
+    ctx: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = R.style.NethCTIButton
+) : MaterialButton(ctx, attrs, defStyleAttr) {
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        val width = measuredWidth
+        val height = measuredHeight
+        val dimen = min(width, height)
+        setMeasuredDimension(dimen, dimen)
+        /* recalculate icon position */
+        iconGravity = ICON_GRAVITY_START
+        iconGravity = ICON_GRAVITY_TEXT_START
+    }
+}
