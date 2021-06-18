b1d0276ea7e37cb2322b6631af7d00173ffb758e
Simone Raffaelli <s.raffaelli@wedoit.io>
2021-06-18 17:43:11 +0200

    Insert background app watcher

---
+++ app/src/main/java/org/linphone/NethCTIApplication.kt
@@ -0,0 +1,53 @@
+package org.linphone
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+import androidx.lifecycle.ProcessLifecycleOwner
+import it.nethesis.utils.AppBackgroundWatcher
+import org.linphone.settings.LinphonePreferences
+
+class NethCTIApplication : Application(), LifecycleObserver {
+
+    companion object {
+        @JvmStatic
+        private lateinit var _instance: NethCTIApplication
+
+        @JvmStatic
+        val instance: NethCTIApplication
+            get() {
+                return _instance
+            }
+    }
+
+    val backgroundWatcher: AppBackgroundWatcher = AppBackgroundWatcher
+
+    override fun onCreate() {
+        super.onCreate()
+        _instance = this
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onMoveToForeground() {
+        // app moved to foreground
+        backgroundWatcher.value = false
+        Log.d("WEDO", "App in foreground.")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun onMoveToBackground() {
+        // app moved to background
+        backgroundWatcher.value = true
+        Log.d("WEDO", "App in background.")
+        //killService
+        LinphoneManager.getLcIfManagerNotDestroyedOrNull()?.let {
+            if (it.calls.isEmpty() && LinphonePreferences.instance().serviceNotificationVisibility && LinphoneService.isReady()
+            ) {
+                LinphoneService.instance().stopSelf()
+            }
+        }
+    }
+}
