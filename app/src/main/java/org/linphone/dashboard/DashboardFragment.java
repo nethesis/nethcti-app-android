03fa9b07ab5faa7ed9270e21445bb4137275db9f
Luca Giorgetti <lucagiorgit95@gmail.com>
2021-07-05 15:32:05 +0200

    Add buttons

--- app/src/main/java/org/linphone/dashboard/DashboardFragment.java
+++ app/src/main/java/org/linphone/dashboard/DashboardFragment.java
@@ -1,7 +1,21 @@
 package org.linphone.dashboard;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.Nullable;
+import org.linphone.R;
 
 public class DashboardFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
+        View view = inflater.inflate(R.layout.dashboard, container, false);
+
+        return view;
+    }
 }
