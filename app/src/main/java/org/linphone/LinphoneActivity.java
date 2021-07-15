115440f8f9977ec7cf9497580b58b6e7509fdc76
Simone Raffaelli <s.raffaelli@wedoit.io>
2021-07-15 10:33:57 +0200

    fix tab bar visibility

--- app/src/main/java/org/linphone/LinphoneActivity.java
+++ app/src/main/java/org/linphone/LinphoneActivity.java
@@ -847,7 +847,7 @@ public class LinphoneActivity extends LinphoneGenericActivity
                 break;
             case DASHBOARD:
                 mFragment = new DashboardFragment();
-                hideTabBar(true);
+                mTabBar.setVisibility(View.GONE); //Force hide tab bar even in tablet mode
                 break;
             default:
                 break;
